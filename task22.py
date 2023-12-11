ba1ce35f83254e3089b496bd357edd7ba222751c
Oleg Garmash <oleg@stavrol.ru>
2023-12-11 17:23:07 +0400

    Выполнил задачи 22 и 24.

---
+++ task22.py
@@ -0,0 +1,37 @@
+'''
+Даны два неупорядоченных набора целых чисел (может быть, с повторениями).
+Выдать без повторений в порядке возрастания все те числа, которые встречаются в обоих наборах. Пользователь вводит 2 числа.
+n - кол-во элементов первого множества.
+m - кол-во элементов второго множества.
+Затем пользователь вводит сами элементы множеств.
+'''
+
+import random
+
+lenlst_1 = int(input("Введите длину списка: "))
+lenlst_2 = int(input("Введите длину списка: "))
+lst_1 = []
+lst_2 = []
+
+for _ in range(lenlst_1):
+    lst_1.append(random.randint(-10, 10))
+
+for _ in range(lenlst_2):
+    lst_2.append(random.randint(-10, 10))
+
+print(lst_1)
+print(set(lst_1))
+print(lst_2)
+print(set(lst_2))
+
+result = list(set(lst_1).intersection(set(lst_2)))
+
+for i in range(len(result)):
+    for n in range(len(result) - 1):
+        if result[i] < result[n]:
+            temp = result[i]
+            result[i] = result[n]
+            result[n] = temp
+
+print(f'Общие значения: {result}')
+print(*result, sep=' ')
